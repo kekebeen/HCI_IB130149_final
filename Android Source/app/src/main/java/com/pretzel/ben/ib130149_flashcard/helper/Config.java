bc1280978f1b874bf6f195a668129da5e68b4ca7
kekebeen <kakosi69@gmail.com>
2018-06-29 04:35:15 +0200

    Added apk file, updated modules

--- Android Source/app/src/main/java/com/pretzel/ben/ib130149_flashcard/helper/Config.java
+++ Android Source/app/src/main/java/com/pretzel/ben/ib130149_flashcard/helper/Config.java
@@ -2,6 +2,6 @@ package com.pretzel.ben.ib130149_flashcard.helper;
 
 public class Config
 {
-    //public static final String urlApi="http://localhost:49239"
-    public static final String urlApi = "http://0d99dbac.ngrok.io/api/";
+    public static final String urlApi="http://localhost:49239";
+    //public static final String urlApi = "http://0d99dbac.ngrok.io/api/";
 }
